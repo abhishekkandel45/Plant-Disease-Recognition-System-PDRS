cf246403ae50bcfae2e7e1caa5c60e3a132354eb
Abhishek Kandel <38427430+abhishekkandel45@users.noreply.github.com>
2022-06-06 23:47:27 +0530

    Update main.py

--- training/main.py
+++ training/main.py
@@ -1,19 +1,17 @@
-from fastapi import FastAPI, UploadFile
-import uvicorn
+from fastapi import FastAPI, UploadFile, File
+from uvicorn import run
 
 app = FastAPI()
 
-@app.get("/ping")
-def ping():
+@app.get("/")
+def root():
     return "Hello!! This is Abhishek's first API"
 
-
-@app.post("/predict")
-async def predict (
-    file: UploadFile = File(...)
+@app.post("/prediction")
+async def predict(
+    file:UploadFile= File(...)
 ):
-    return {"result": "success"} 
-
+   pass
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000)
+    run(app, host='localhost', port=8000)
