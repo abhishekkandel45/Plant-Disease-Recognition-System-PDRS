7be793630f37d41fae13aaa844e623ee5a89331c
Abhishek Kandel <38427430+abhishekkandel45@users.noreply.github.com>
2022-06-07 07:39:22 +0530

    Update main.py

--- training/main.py
+++ training/main.py
@@ -1,17 +1,73 @@
-from fastapi import FastAPI, UploadFile, File
-from uvicorn import run
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return "Hello!! This is Abhishek's first API"
 
-@app.post("/prediction")
+#Model Load
+MODEL = tf.keras.models.load_model("../API/model.h5")
+CLASS_NAMES =['Apple___Apple_scab',
+ 'Apple___Black_rot',
+ 'Apple___Cedar_apple_rust',
+ 'Apple___healthy',
+ 'Blueberry___healthy',
+ 'Cherry_(including_sour)___Powdery_mildew',
+ 'Cherry_(including_sour)___healthy',
+ 'Corn_(maize)___Cercospora_leaf_spot',
+ 'Corn_(maize)___Common_rust_',
+ 'Corn_(maize)___Northern_Leaf_Blight',
+ 'Corn_(maize)___healthy',
+ 'Grape___Black_rot',
+ 'Grape___Esca_(Black_Measles)',
+ 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+ 'Grape___healthy',
+ 'Orange___Haunglongbing_(Citrus_greening)',
+ 'Peach___Bacterial_spot',
+ 'Peach___healthy',
+ 'Pepper,_bell___Bacterial_spot',
+ 'Pepper,_bell___healthy',
+ 'Potato___Early_blight',
+ 'Potato___Late_blight',
+ 'Potato___healthy',
+ 'Raspberry___healthy',
+ 'Soybean___healthy',
+ 'Squash___Powdery_mildew',
+ 'Strawberry___Leaf_scorch',
+ 'Strawberry___healthy',
+ 'Tomato___Bacterial_spot',
+ 'Tomato___Early_blight',
+ 'Tomato___Late_blight',
+ 'Tomato___Leaf_Mold',
+ 'Tomato___Septoria_leaf_spot',
+ 'Tomato___Spider_mites',
+ 'Tomato___Target_Spot',
+ 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+ 'Tomato___Tomato_mosaic_virus',
+ 'Tomato___healthy']
+
+@app.get("/ping")
+async def ping():
+    return "Hello, This is my first API"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
 async def predict(
-    file:UploadFile= File(...)
+    file: UploadFile = File(...)
 ):
-   pass
+    image = read_file_as_image(await file.read())
+    img_batch=np.expand_dims(image,0)
+    prediction = MODEL.predict(img_batch)
+    pass
+
+
 
 if __name__ == "__main__":
-    run(app, host='localhost', port=8000)
+    uvicorn.run(app, host='localhost', port=8000)
