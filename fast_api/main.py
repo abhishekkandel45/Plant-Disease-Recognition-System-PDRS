6c79e97c332803cd16dac8a91dc12bf4b1a41a62
Abhishek Kandel <38427430+abhishekkandel45@users.noreply.github.com>
2022-06-16 12:41:19 +0530

    Update main.py

--- fast_api/main.py
+++ fast_api/main.py
@@ -5,6 +5,9 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
+ import os
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 
 app = FastAPI()
 
@@ -53,21 +56,6 @@ CLASS_NAMES =['Apple___Apple_scab',
 @app.get("/ping")
 async def ping():
     return "Hello, This is my first API"
-
-def read_file_as_image(data) -> np.ndarray:
-    image = np.array(Image.open(BytesIO(data)))
-    return image
-
-@app.post("/predict")
-async def predict(
-    file: UploadFile = File(...)
-):
-    image = read_file_as_image(await file.read())
-    img_batch=np.expand_dims(image,0)
-    prediction = MODEL.predict(img_batch)
-    pass
-
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000)
+    
+    if __name__ == "__main__":
+        uvicorn.run(app, host="localhost", port=8000)
